a89b84c2f2da1c5d93a25e15f9f4e3afe842f9cd
LieneLittlebird <liene.putnina20@gmail.com>
2021-02-25 22:12:42 +0200

    dictionaries learned

---
+++ dictionaries.py
@@ -0,0 +1,56 @@
+# A collection which is unordered, changeable and indexed. Nu duplicates
+#Similar to object literal
+
+#Create a dictionary - an object in JS
+person = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "age": 30
+}
+print(person, type(person))
+
+#Use constructor
+# person2 = dict(firs_name = "Sara", last_name = "Williams")
+
+# print(person2, type(person2))
+
+#Get value
+print(person["first_name"]) #More common
+print(person.get("last_name"))
+
+#Add key/value
+person["phone_number"] = "555-555-555"
+
+print(person)
+
+#Get dict keys
+print(person.keys())
+
+#Get dict items
+print(person.items())
+
+#Copy a dict
+person2 = person.copy()
+person2["city"] = "Boston"
+
+print(person2)
+
+#Remove item
+del(person["age"])
+person.pop("phone_number")
+
+#Clear
+person.clear()
+
+#Get length
+print(len(person2))
+
+#List of dict
+people = [
+    {"name": "Martha", "age": 30},
+    {"name": "Kevin", "age": 25}
+]
+
+print(people[1]["name"])
+# print(person)
+
