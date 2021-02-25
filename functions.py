585fd669c7d61600c0a8e2686b27d6a83f719ecc
LieneLittlebird <liene.putnina20@gmail.com>
2021-02-25 16:55:34 +0200

    started functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+# Create a function
+# In Python we use tabs and spaces instead of curly brackets
+def sayHello(name="Sam"):  # Add a default name
+    print(f"Hello {name}")
+
+
+# sayHello()
+
+# Return values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+num = (getSum(3, 4))
+print(num)
