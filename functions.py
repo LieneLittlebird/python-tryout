b071faa307357323baca64a692786ec096f3ff39
LieneLittlebird <liene.putnina20@gmail.com>
2021-02-25 20:34:05 +0200

    finished function tutorial

--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@
 def sayHello(name="Sam"):  # Add a default name
     print(f"Hello {name}")
 
-
 # sayHello()
 
 # Return values
@@ -11,6 +10,15 @@ def getSum(num1, num2):
     total = num1 + num2
     return total
 
-
 num = (getSum(3, 4))
-print(num)
+# print(num)
+
+#Lambda function - a small anonymous function. 
+# Can take in any number of arguments, but has only one expression.
+#Similar to arrow functions in JS
+
+getSum2 = lambda num1, num2: num1 + num2;
+
+# print(getSum(10, 3));
+
+
